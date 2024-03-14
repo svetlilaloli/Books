8bc016dbc9f1f195e216015f837c8726ce225c42
svetlilaloli <svetlilaloli@gmail.com>
2024-03-14 07:20:27 +0000

    updated to terminate

--- src/server.js
+++ src/server.js
@@ -42,6 +42,8 @@ server.delete('/books/:id', (req, res) => {
     }
 });
 
-server.listen(3000, () => console.log('Server is up and running on http://localhost:3000'));
+if (require.main === module) {
+    server.listen(3000, () => console.log('Server is up and running on http://localhost:3000'));
+}
 
 module.exports = server; // Exporting for testing
