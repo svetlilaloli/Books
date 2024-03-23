e69b9281c3c94a9a2b74394d0097e64d25c2ad51
svetlilaloli <svetlilaloli@gmail.com>
2024-03-23 08:36:33 +0000

    negative tests added

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,40 +4,75 @@ import * as chai from 'chai';
 
 const expect = chai.expect;
 
-describe('Books API', () => {
-    it('should return an empty array when no books are added', async () => {
+describe('Verify existing book API tests', () => {
+    const book = {
+        id: '1', 
+        title: 'Test Book', 
+        author: 'Test Author'
+    };
+    
+    it('Should return an empty array when no books are added', async () => {
         const res = await request(server).get('/books');
         expect(res.status).to.equal(200);
         expect(res.body).to.be.an('array').that.is.empty;
     });
         
-    it('should add a new book', async () => {
-        const newBook = { id: '1', title: 'Test Book', author: 'Test Author' };
+    it('Should POST a book', async () => {
         const res = await request(server)
             .post('/books')
-            .send(newBook);
+            .send(book);
         expect(res.status).to.equal(201);
-        expect(res.body).to.deep.equal(newBook);
+        expect(res.body).to.deep.equal(book);
     });
 
-    it('should return a book by id', async () => {
-        const testBook = { id: '1', title: 'Test Book', author: 'Test Author' };
-        const res = await request(server).get('/books/1');
+    it('Should GET all books', async () => {
+        const res = await request(server).get('/books/');
         expect(res.status).to.equal(200);
-        expect(res.body).to.deep.equal(testBook);
+        expect(res.body).to.be.a('array');
     });
 
-    it('should update a book by id', async () => {
-        const updatedBook = { id: '1', title: 'Updated Test Book', author: 'Updated Test Author' };
-        const res = await request(server)
-            .put('/books/1')
-            .send(updatedBook);
+    it('Should GET a book by Id', async () => {
+        const res = await request(server).get(`/books/${book.id}`);
         expect(res.status).to.equal(200);
-        expect(res.body).to.deep.equal(updatedBook);
+        expect(res.body).to.deep.equal(book);
     });
 
-    it('should delete a book by id', async () => {
-        const res = await request(server).delete('/books/1');
+    it('Should PUT (update) a book by Id', async () => {
+        book.title = 'Updated Test Book';
+        book.author = 'Updated Test Author';
+        const res = await request(server).put(`/books/${book.id}`).send(book);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.deep.equal(book);
+    });
+
+    it('Should DELETE a book by Id', async () => {
+        const res = await request(server).delete(`/books/${book.id}`);
         expect(res.status).to.equal(204);
+    });
+});
+
+describe('Verify non-existing book API tests', () => {
+    const noBook = {
+        id: '9999',
+        title: 'Some title',
+        author: 'Some author'
+    };
+
+    it('Should return 404 when trying to GET non-existing book by Id', async () => {
+        const res = await request(server).get(`/books/${noBook.id}`);
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('Book not found');
+    });
+
+    it('Should return 404 when trying to PUT (update) non-existing book by Id', async () => {
+        const res = await request(server).put(`/books/${noBook.id}`).send(noBook);
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('Book not found');
+    });
+
+    it('Should return 404 when trying to DELETE non-existing book by Id', async () => {
+        const res = (await request(server).delete(`/books/${noBook.id}`));
+        expect(res.status).to.equal(404);
+        expect(res.body.message).to.equal('Book not found');
     });
 });
